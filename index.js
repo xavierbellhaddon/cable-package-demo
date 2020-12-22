2a740d97bebaf488a0b59847df0fe51beab3bc1d
Austin Bell <snarkeet@gmail.com>
2020-12-21 22:52:00 -0800

    added running total

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const checkbox = document.querySelectorAll(".package-option");
+const total = document.querySelector(".total");
+
+let runningTotal = 0;
 
 checkbox.forEach((checkbox) => {
   checkbox.addEventListener("change", function () {
     if (this.checked) {
       console.log("Checked");
+      runningTotal += parseInt(this.value);
     } else {
       console.log("Unchecked");
+      runningTotal -= parseInt(this.value)
     }
+    total.innerHTML = runningTotal
   });
 });
